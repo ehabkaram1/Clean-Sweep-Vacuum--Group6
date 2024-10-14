9924182a816c2d4d75c89610c931adac2fc48447
MaherVahora <mvahora5@depaul.edu>
2024-10-14 13:01:31 -0500

    Update Main.java

--- ChargingStation/Main.java
+++ ChargingStation/Main.java
@@ -1,5 +1,21 @@
-package ChargingStation;
+package FloorPlan;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        // Create a 5x5 floor plan
+        FloorPlan floorPlan = new FloorPlan(5, 5);
+
+        // Starting position of the robot
+        int startX = 0, startY = 0;
+
+        // Find the nearest charging station
+        Cell nearestChargingStation = floorPlan.findNearestChargingStation(startX, startY);
+
+        if (nearestChargingStation != null) {
+            System.out.println("Nearest charging station found at: (" + nearestChargingStation.getX() + ", " + nearestChargingStation.getY() + ")");
+        } else {
+            System.out.println("No charging station found.");
+        }
+    }
 }
+
