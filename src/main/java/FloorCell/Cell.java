d754a675f28a9ccc31d453fd83f0f23924d0361f
Hardik Panchal <85635598+hardy-12@users.noreply.github.com>
2024-10-28 18:50:27 +0000

    updated Cell.java

--- src/main/java/FloorCell/Cell.java
+++ src/main/java/FloorCell/Cell.java
@@ -1,5 +1,16 @@
 package FloorCell;
+ 
+import FloorPlan.TileType;
 
 public class Cell {
-    
+
+  public SideType sideN;
+  public SideType sideS;
+  public SideType sideW;
+  public SideType sideE;
+
+  private TileType surface;
+  private int dirtAmount;
+
+  
 }
