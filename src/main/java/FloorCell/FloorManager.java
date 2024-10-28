85f1efc4e86b0df52ba207797b3db5b67bb6bcfa
Hardik Panchal <85635598+hardy-12@users.noreply.github.com>
2024-10-28 19:38:24 +0000

    added fucntionality to FloorManager.java

---
+++ src/main/java/FloorCell/FloorManager.java
@@ -0,0 +1,12 @@
+package FloorCell;
+
+public class FloorManager {
+
+    public CellNode first;
+  
+    public FloorManager() {
+      this.first = null;
+    }
+  
+    public void addFirstCell(CellNode fir) {}
+  }
