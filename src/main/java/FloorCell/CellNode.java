9285e9d36e378040e4a0db359dba36c30bc5db63
Hardik Panchal <85635598+hardy-12@users.noreply.github.com>
2024-10-28 19:11:06 +0000

    updated CellNode.java

---
+++ src/main/java/FloorCell/CellNode.java
@@ -0,0 +1,16 @@
+package FloorCell;
+
+import FloorCell.*;;
+
+public class CellNode {
+  private SideType sideN;
+  private SideType sideS;
+  private SideType sideW;
+  private SideType sideE;
+
+  private CellNode nodeN;
+  private CellNode nodeS;
+  private CellNode nodeW;
+  private CellNode nodeE;
+
+}
