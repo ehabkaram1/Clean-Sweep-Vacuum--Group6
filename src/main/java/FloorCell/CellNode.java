9bddd6ff26a0c924d417f3198072116727316670
Hardik Panchal <85635598+hardy-12@users.noreply.github.com>
2024-10-28 19:12:07 +0000

    updated CellNode.java

--- src/main/java/FloorCell/CellNode.java
+++ src/main/java/FloorCell/CellNode.java
@@ -13,4 +13,8 @@ public class CellNode {
   private CellNode nodeW;
   private CellNode nodeE;
 
+  private SurfaceType surface;
+  private int dirtAmount;
+  private int[] coordinate = new int[2];
+
 }
