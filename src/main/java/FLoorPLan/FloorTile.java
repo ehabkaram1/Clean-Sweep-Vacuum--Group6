18661f9fce3e8106161197050e589dcea3719cf6
Hardik Panchal <85635598+hardy-12@users.noreply.github.com>
2024-10-28 20:00:16 +0000

    Updated FloorTile.java

--- src/main/java/FLoorPLan/FloorTile.java
+++ src/main/java/FLoorPLan/FloorTile.java
@@ -1,5 +1,33 @@
-package src.main.java.FLoorPLan;
+package FLoorPLan;
 
-public class FloorTile {
-    
-}
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.io.Serializable;
+
+@XmlRootElement(name = "node")
+@XmlAccessorType(XmlAccessType.PROPERTY)
+public class FloorTile implements Serializable {
+  private static final long serialVersionUID = 1L;
+
+  public FloorTile north;
+  public FloorTile south;
+  public FloorTile east;
+  public FloorTile west;
+  public int _x;
+  public int _y;
+
+  public int G;
+  public int H;
+  public int F;
+  public FloorTile parent;
+
+  TileType surfaceType;
+  boolean isClean;
+  boolean accessable;
+  boolean chargingStation;
+  int unitsOfDirt;
+
+  public FloorTile() {
+    super();
+  }
